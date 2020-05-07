bc2b28a1fe65c18c77408e8712497129aa11c959
williamkennedy <wkennedy8@yahoo.com>
2020-05-07 12:48:03 -0400

    add scroll to blog preview

--- components/Blog/BlogCard.js
+++ components/Blog/BlogCard.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import Link from "next/link"
+import React, { useState } from 'react'
+import Link from 'next/link'
 import BlogCardTwo from './BlogCardTwo'
 
 const BlogCard = (props) => {
@@ -12,36 +12,34 @@ const BlogCard = (props) => {
 		<section className="blog-section ptb-100">
 			{pageOne && (
 				<div className="container">
-			{
-				props.pageTitle && (
+					{props.pageTitle && (
+						<div className="section-title">
+							<h2>Thank You</h2>
+							<br />
+							<span style={{ color: '#737b9a' }}>{this.props.pageTitle}</span>
+						</div>
+					)}
 					<div className="section-title">
-						<h2>Thank You</h2>
-						<br/>
-						<span style={{color: '#737b9a'}}>{this.props.pageTitle}</span>
+						<span>Read Our Latest Post</span>
+						<h2>Blog & Updates</h2>
+						<br />
+						<div>
+							<p>
+								Data is constantly changing. At Daasly, we believe in having a
+								deep knowledge of the past and a keen sense for new and future
+								methods so we can improve and evolve as data changes. Stay
+								updated with our latest posts.
+							</p>
+						</div>
 					</div>
-				)
-			}
-				<div className="section-title">
-					<span>Read Our Latest Post</span>
-					<h2>Blog & Updates</h2>
-					<br/>
-					<div>
-						<p>
-							Data is constantly changing. At Daasly, we believe in having a
-							deep knowledge of the past and a keen sense for new and future
-							methods so we can improve and evolve as data changes. Stay
-							updated with our latest posts.
-						</p>
-					</div>
-				</div>
 
-				<div className="row">
-					<div className="col-lg-4 col-md-6">
+					<div className="row">
+						<div className="col-lg-4 col-md-6">
 							<div className="blog-item">
 								<Link href="/blog/covid-19">
 									<a>
 										<img
-											src={require("../../images/blog/blog2.png")}
+											src={require('../../images/blog/blog2.png')}
 											alt="image"
 										/>
 									</a>
@@ -76,7 +74,9 @@ const BlogCard = (props) => {
 									</h3>
 
 									<p>
-										This is no time to panic, but it certainly is a good time to reassess your data strategies as you set up your home office.
+										This is no time to panic, but it certainly is a good time to
+										reassess your data strategies as you set up your home
+										office.
 									</p>
 
 									<Link href="/blog/covid-19">
@@ -93,7 +93,7 @@ const BlogCard = (props) => {
 								<Link href="/blog/real-time-alerts">
 									<a>
 										<img
-											src={require("../../images/blog/blog1.png")}
+											src={require('../../images/blog/blog1.png')}
 											alt="image"
 										/>
 									</a>
@@ -128,7 +128,8 @@ const BlogCard = (props) => {
 									</h3>
 
 									<p>
-										Check out this guide, and soon you'll become a real-time data expert capable of solving lots of different problems.
+										Check out this guide, and soon you'll become a real-time
+										data expert capable of solving lots of different problems.
 									</p>
 
 									<Link href="/blog/real-time-alerts">
@@ -146,7 +147,7 @@ const BlogCard = (props) => {
 								<Link href="/blog/ga-network-dimensions">
 									<a>
 										<img
-											src={require("../../images/blog/blog6.png")}
+											src={require('../../images/blog/blog6.png')}
 											alt="image"
 										/>
 									</a>
@@ -181,25 +182,26 @@ const BlogCard = (props) => {
 									</h3>
 
 									<p>
-										Mourning the Death of “Service Provider” & “Network Domain” in Google Analytics.
+										Mourning the Death of “Service Provider” & “Network Domain”
+										in Google Analytics.
 									</p>
 
-								<Link href="/blog/ga-network-dimensions">
-									<a className="read-more">
-										Read More
-										<i className="fa fa-chevron-right"></i>
-									</a>
-								</Link>
+									<Link href="/blog/ga-network-dimensions">
+										<a className="read-more">
+											Read More
+											<i className="fa fa-chevron-right"></i>
+										</a>
+									</Link>
+								</div>
 							</div>
 						</div>
-					</div>
 
-					<div className="col-lg-4 col-md-6">
+						<div className="col-lg-4 col-md-6">
 							<div className="blog-item">
 								<Link href="/blog/google-tag-manager">
 									<a>
 										<img
-											src={require("../../images/blog/blog5.png")}
+											src={require('../../images/blog/blog5.png')}
 											alt="image"
 										/>
 									</a>
@@ -234,7 +236,8 @@ const BlogCard = (props) => {
 									</h3>
 
 									<p>
-										There are well over 11 reasons why you should use Google Tag Manager, these are just the tip.
+										There are well over 11 reasons why you should use Google Tag
+										Manager, these are just the tip.
 									</p>
 
 									<Link href="/blog/google-tag-manager">
@@ -252,7 +255,7 @@ const BlogCard = (props) => {
 								<Link href="/blog/domo-and-aws">
 									<a>
 										<img
-											src={require("../../images/blog/blog4.png")}
+											src={require('../../images/blog/blog4.png')}
 											alt="image"
 										/>
 									</a>
@@ -282,14 +285,14 @@ const BlogCard = (props) => {
 
 									<h3>
 										<Link href="/blog/domo-and-aws">
-											<a>
-												Domo Partners with AWS
-											</a>
+											<a>Domo Partners with AWS</a>
 										</Link>
 									</h3>
 
 									<p>
-										The new Domo BI platform update features a new collaboration with AWS to enable users to work with third-party data and a low-code tool for developers.
+										The new Domo BI platform update features a new collaboration
+										with AWS to enable users to work with third-party data and a
+										low-code tool for developers.
 									</p>
 
 									<Link href="/blog/domo-and-aws">
@@ -301,13 +304,13 @@ const BlogCard = (props) => {
 								</div>
 							</div>
 						</div>
-						
+
 						<div className="col-lg-4 col-md-6">
 							<div className="blog-item">
 								<Link href="/blog/data-buy-in-blog">
 									<a>
 										<img
-											src={require("../../images/blog/blog2.png")}
+											src={require('../../images/blog/blog2.png')}
 											alt="image"
 										/>
 									</a>
@@ -342,7 +345,10 @@ const BlogCard = (props) => {
 									</h3>
 
 									<p>
-										Data professionals face an endless challenge to get the buy-in of management and IT to provide the technology and resources needed to efficiently and effectively analyze their constantly growing enterprise data stores.
+										Data professionals face an endless challenge to get the
+										buy-in of management and IT to provide the technology and
+										resources needed to efficiently and effectively analyze
+										their constantly growing enterprise data stores.
 									</p>
 
 									<Link href="/blog/data-buy-in-blog">
@@ -354,60 +360,66 @@ const BlogCard = (props) => {
 								</div>
 							</div>
 						</div>
-
+					</div>
 				</div>
-			</div>
 			)}
-			{
-				pageTwo && !pageOne && (
-					<BlogCardTwo/>
-				)
-			}
+			{pageTwo && !pageOne && <BlogCardTwo />}
 			{/* Pagination */}
-					<div className="col-lg-12 col-md-12">
-						<div className="pagination-area">
-							
-								<span className="prev page-numbers" onClick={() => {
-									if (pageOne) {
-										setPageTwo(false)
-										setPageThree(false)
-										setPageFour(false)
-									} else if (pageTwo) {
-										setPageOne(true)
-										setPageTwo(false)
-										setPageThree(false)
-										setPageFour(false)
-									} else if (pageThree){
-										setPageFour(false)
-										setPageThree(false)
-										setPageTwo(true)
-										setPageOne(false)
-									} else {
-										setPageFour(false)
-										setPageThree(true)
-										setPageTwo(false)
-										setPageOne(false)
-									}
-								}}>
-									<i className="fas fa-angle-double-left"></i>
-								</span>
-						
-
-							<span onClick={() => {
+			<div className="col-lg-12 col-md-12">
+				<div className="pagination-area">
+					<span
+						className="prev page-numbers"
+						onClick={() => {
+							if (pageOne) {
+								setPageTwo(false)
+								setPageThree(false)
+								setPageFour(false)
+							} else if (pageTwo) {
 								setPageOne(true)
 								setPageTwo(false)
-								// setPageThree(false)
-								// setPageFour(false)
-							}}className={pageOne ? "page-numbers current" : "page-numbers"}>1</span>
-
-							<span onClick={() => {
-								setPageOne(false)
+								setPageThree(false)
+								setPageFour(false)
+							} else if (pageThree) {
+								setPageFour(false)
+								setPageThree(false)
 								setPageTwo(true)
-								// setPageThree(false)
-								// setPageFour(false)
-							}}className={pageTwo ? "page-numbers current" : "page-numbers"}>2</span>
+								setPageOne(false)
+							} else {
+								setPageFour(false)
+								setPageThree(true)
+								setPageTwo(false)
+								setPageOne(false)
+							}
+						}}
+					>
+						<i className="fas fa-angle-double-left"></i>
+					</span>
 
-							{/* <span onClick={() => {
+					<span
+						onClick={() => {
+							setPageOne(true)
+							setPageTwo(false)
+							// setPageThree(false)
+							// setPageFour(false)
+						}}
+						className={pageOne ? 'page-numbers current' : 'page-numbers'}
+					>
+						1
+					</span>
+
+					<span
+						onClick={() => {
+							setPageOne(false)
+							setPageTwo(true)
+							// setPageThree(false)
+							// setPageFour(false)
+						}}
+						className={pageTwo ? 'page-numbers current' : 'page-numbers'}
+					>
+						2
+					</span>
+
+					{/* <span onClick={() => {
 								setPageOne(false)
 								setPageTwo(false)
 								setPageThree(true)
@@ -421,37 +433,37 @@ const BlogCard = (props) => {
 								setPageFour(true)
 							}}className={pageFour ? "page-numbers current" : "page-numbers"}>4</span> */}
 
-							
-								<span className="next page-numbers" onClick={() => {
-									if (pageOne) {
-										setPageOne(false)
-										setPageTwo(true)
-										// setPageThree(false)
-										// setPageFour(false)
-									} else if (pageTwo) {
-										setPageOne(false)
-										setPageTwo(true)
-										// setPageThree(true)
-										// setPageFour(false)
-									} 
-									// else if (pageThree){
-									// 	setPageFour(true)
-									// 	setPageThree(false)
-									// 	setPageTwo(false)
-									// 	setPageOne(false)
-									// } else {
-									// 	setPageFour(true)
-									// 	setPageThree(false)
-									// 	setPageTwo(false)
-									// 	setPageOne(false)
-									// }
-								}}>
-									<i className="fas fa-angle-double-right"></i>
-								</span>
-							
-						</div>
-					</div>
-			
+					<span
+						className="next page-numbers"
+						onClick={() => {
+							if (pageOne) {
+								setPageOne(false)
+								setPageTwo(true)
+								// setPageThree(false)
+								// setPageFour(false)
+							} else if (pageTwo) {
+								setPageOne(false)
+								setPageTwo(true)
+								// setPageThree(true)
+								// setPageFour(false)
+							}
+							// else if (pageThree){
+							// 	setPageFour(true)
+							// 	setPageThree(false)
+							// 	setPageTwo(false)
+							// 	setPageOne(false)
+							// } else {
+							// 	setPageFour(true)
+							// 	setPageThree(false)
+							// 	setPageTwo(false)
+							// 	setPageOne(false)
+							// }
+						}}
+					>
+						<i className="fas fa-angle-double-right"></i>
+					</span>
+				</div>
+			</div>
 		</section>
 	)
 }
