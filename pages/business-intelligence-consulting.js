b459e3a69738bec5644e01a6c54da7624e311369
williamkennedy <wkennedy8@yahoo.com>
2020-05-28 23:02:26 -0400

    change last bullet text

--- pages/business-intelligence-consulting.js
+++ pages/business-intelligence-consulting.js
@@ -26,7 +26,7 @@ class ServiceDetails extends Component {
                    subtext="Daasly is a proud Certified Domo Platform, Professional Services and Technical Consultant Partners. Domo is a game-changing, cloud-based platform specializing in business intelligence tools and data visualization. Domo can be used to:"
                    listItems={{ one: 'Connect Platforms', two: 'Align Metrics', three: "Measure Data", four: "Analyze Information", five: "Optimize Performance", six: "Predict Results", seven: "Visualize Information", eight: "Automate Processes"}}
                    headerTwo="What We Do"
-                   secondList={{one: 'Fast delivery of data-driven business analytics', two: 'Easy-to-use, customized Business Intelligence dashboards', three: 'Streamlined and automated data analytics and reporting', four: 'Predictive forecasting through analysis of business data', five: 'Share on insights and data to collaborate with your team'}}
+                   secondList={{one: 'Fast delivery of data-driven business analytics', two: 'Easy-to-use, customized Business Intelligence dashboards', three: 'Streamlined and automated data analytics and reporting', four: 'Predictive forecasting through analysis of business data', five: 'Share our insights and data to collaborate with your team'}}
                    domo={true}
                    imageOne={require('../images/projects/project1.png')}
                    imageTwo={require('../images/about.png')}
