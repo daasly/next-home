dcfd7f14e26286ecd18b25bf158e1962e6209cb3
wkennedy210@gmail.com <6560256wK1>
2020-06-23 16:14:35 -0400

    add underline and bold to links, update CTA on blogs

--- pages/blog/ga-network-dimensions.js
+++ pages/blog/ga-network-dimensions.js
@@ -9,21 +9,21 @@ class GANetworkDimensions extends Component {
         return (
             <React.Fragment>
                 <Navbar2 />
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <BlogDetailsContent 
-                  date='March 04, 2020' 
-                  author='Emilio Izquierdo' 
-                  title="Removed Dimensions from Google Analytics"
-                  quote="Mourning the Death of “Service Provider” & “Network Domain” in Google Analytics"
-                  text2={true}
-                  text="Were you affected by Google’s decisions to remove the Service Provider and Network Domain dimensions from Google Analytics (GA)? These dimensions are commonly used to filter out internal network traffic that may skew your analytics. Have no fear, there are other solutions! One solution is to assign an identifier to the user agent field on your corporate browsers, this dimension can be picked up as a custom dimension and used to filter out your internal traffic. 
-                      Need help? Daasly is here for you!"
-                  articleLink="//www.clickinsight.ca/blog/service-provider-not-set-google-analytics"
-                  shareLink="https://www.linkedin.com/shareArticle?mini=true&url=https://daasly.com/blog/ga-network-dimensions&title=&summary=&source="
-                  />
+                <br />
+                <br />
+                <br />
+                <br />
+                <BlogDetailsContent
+                    date='March 04, 2020'
+                    author='Emilio Izquierdo'
+                    title="Removed Dimensions from Google Analytics"
+                    quote="Mourning the Death of “Service Provider” & “Network Domain” in Google Analytics"
+                    text2={true}
+                    ctaGADimensions={true}
+                    text="Were you affected by Google’s decisions to remove the Service Provider and Network Domain dimensions from Google Analytics (GA)? These dimensions are commonly used to filter out internal network traffic that may skew your analytics. Have no fear, there are other solutions! One solution is to assign an identifier to the user agent field on your corporate browsers, this dimension can be picked up as a custom dimension and used to filter out your internal traffic."
+                    articleLink="//www.clickinsight.ca/blog/service-provider-not-set-google-analytics"
+                    shareLink="https://www.linkedin.com/shareArticle?mini=true&url=https://daasly.com/blog/ga-network-dimensions&title=&summary=&source="
+                />
                 <Footer />
             </React.Fragment>
         );
--- pages/blog/google-tag-manager.js
+++ pages/blog/google-tag-manager.js
@@ -9,20 +9,21 @@ class GoogleTagManager extends Component {
         return (
             <React.Fragment>
                 <Navbar2 />
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <BlogDetailsContent 
-                  date='Febraury 27, 2020' 
-                  author='Emilio Izquierdo' 
-                  title="Google Tag Manager is Your Friend"
-                  quote="There are well over 11 reasons why you should use Google Tag Manager, these are just the tip."
-                  text2={true}
-                  text="Analytics Mania did a great job explaining the basics of what Google Tag Manager (GTM) is and why you should be using this powerful tool. In short, GTM empowers marketing teams to take control of tagging code, creating a clear distinction between the source code of your web applications and tagging events used for analytics in one place. The basic benefits of GTM are deployable by just about anyone who is willing to dedicate a little time to learn. Users with development experience can leverage GTM at much greater lengths. Let me know if Daasly can help you!"
-                  articleLink="//www.analyticsmania.com/post/reasons-why-you-should-use-google-tag-manager/"
-                  shareLink="https://www.linkedin.com/shareArticle?mini=true&url=https://daasly.com/blog/google-tag-manager&title=&summary=&source="
-                  />
+                <br />
+                <br />
+                <br />
+                <br />
+                <BlogDetailsContent
+                    date='Febraury 27, 2020'
+                    author='Emilio Izquierdo'
+                    title="Google Tag Manager is Your Friend"
+                    quote="There are well over 11 reasons why you should use Google Tag Manager, these are just the tip."
+                    text2={true}
+                    ctaGTM={true}
+                    text="Analytics Mania did a great job explaining the basics of what Google Tag Manager (GTM) is and why you should be using this powerful tool. In short, GTM empowers marketing teams to take control of tagging code, creating a clear distinction between the source code of your web applications and tagging events used for analytics in one place. The basic benefits of GTM are deployable by just about anyone who is willing to dedicate a little time to learn. Users with development experience can leverage GTM at much greater lengths."
+                    articleLink="//www.analyticsmania.com/post/reasons-why-you-should-use-google-tag-manager/"
+                    shareLink="https://www.linkedin.com/shareArticle?mini=true&url=https://daasly.com/blog/google-tag-manager&title=&summary=&source="
+                />
                 <Footer />
             </React.Fragment>
         );
